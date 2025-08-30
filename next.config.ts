be88f55a5f92af704f1d578b876f142675d8489c
Sakya Rasadi <89144593+sakyarasadi@users.noreply.github.com>
2025-08-30 09:40:38 +0530

    0.0.32

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds:true,
   },
   // Netlify specific configuration
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  }
+  serverExternalPackages: ['@prisma/client', 'prisma']
 };
 
 export default nextConfig;
